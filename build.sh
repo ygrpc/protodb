ab46d21b8cd4317a4df9afaedf53c09c78da27f6
yangjun <290968698@qq.com>
2024-06-02 20:00:44 +0800

    chore: sync protodb.proto to ygrpctest locally

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 protoc --go_out=. protodb.proto
 mv -f github.com/ygrpc/protodb/protodb.pb.go .
 rm -rf github.com
+
+if [ -d "../ygrpctest/cmd/protodb" ]; then
+  echo "sync protodb.proto to ../ygrpctest/cmd/protodb/"
+  rsync -ac --stats protodb.proto ../ygrpctest/cmd/protodb/ | grep files
+fi
