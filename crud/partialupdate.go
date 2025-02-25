9ad412014f7048c101cd1425bb1ad746a4878de4
yangjun <290968698@qq.com>
2025-02-25 12:02:18 +0800

    impl DbUpdatePartialReturnOldAndNew by cursor

--- crud/partialupdate.go
+++ crud/partialupdate.go
@@ -3,10 +3,11 @@ package crud
 import (
 	"database/sql"
 	"fmt"
+	"strings"
+
 	"github.com/ygrpc/protodb"
 	"github.com/ygrpc/protodb/pdbutil"
 	"github.com/ygrpc/protodb/protosql"
-	"strings"
 
 	"github.com/ygrpc/protodb/sqldb"
 	"google.golang.org/protobuf/proto"
@@ -95,6 +96,48 @@ func dbUpdatePartialReturnNew(db *sql.DB, msg proto.Message, updateFields []stri
 	err = DbScan2ProtoMsg(rows, returnMsg, nil, msgFieldsMap)
 
 	return returnMsg, err
+}
+
+// DbUpdatePartialReturnOldAndNew update a message in db and return both old and new messages
+func DbUpdatePartialReturnOldAndNew(db *sql.DB, msg proto.Message, updateFields []string, dbschema string) (oldMsg proto.Message, newMsg proto.Message, err error) {
+	msgPm := msg.ProtoReflect()
+	msgDesc := msgPm.Descriptor()
+	msgFieldDescs := msgDesc.Fields()
+	tableName := string(msgDesc.Name())
+
+	return dbUpdatePartialReturnOldAndNew(db, msg, updateFields, dbschema, tableName, msgDesc, msgFieldDescs)
+}
+
+// dbUpdatePartialReturnOldAndNew updates a message in db and returns both old and new messages
+func dbUpdatePartialReturnOldAndNew(db *sql.DB, msg proto.Message, updateFields []string, dbschema string, tableName string,
+	msgDesc protoreflect.MessageDescriptor,
+	msgFieldDescs protoreflect.FieldDescriptors) (oldMsg proto.Message, newMsg proto.Message, err error) {
+
+	dbdialect := sqldb.GetDBDialect(db)
+
+	sqlStr, sqlVals, err := dbBuildSqlUpdatePartialOldAndNew(msg, updateFields, dbschema, tableName, msgDesc, msgFieldDescs, dbdialect)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rows, err := db.Query(sqlStr, sqlVals...)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		return nil, nil, sql.ErrNoRows
+	}
+
+	oldMsg = msg.ProtoReflect().New().Interface()
+	newMsg = msg.ProtoReflect().New().Interface()
+
+	msgFieldsMap := pdbutil.BuildMsgFieldsMap(nil, msgDesc.Fields(), true)
+
+	err = DbScan2ProtoMsgx2(rows, oldMsg, newMsg, nil, msgFieldsMap)
+
+	return oldMsg, newMsg, err
 }
 
 // dbBuildSqlUpdatePartial build sql update statement
@@ -241,4 +284,161 @@ func dbBuildSqlUpdatePartial(msgobj proto.Message, updateFields []string, dbsche
 
 	return sqlStr, sqlVals, nil
 
+}
+
+// dbBuildSqlUpdatePartialOldAndNew build sql update statement and return old and new row
+func dbBuildSqlUpdatePartialOldAndNew(msgobj proto.Message, updateFields []string, dbschema string, tableName string,
+	msgDesc protoreflect.MessageDescriptor,
+	msgFieldDescs protoreflect.FieldDescriptors,
+	dbdialect sqldb.TDBDialect) (sqlStr string, sqlVals []interface{}, err error) {
+
+	dbtableName := tableName
+	if len(dbschema) == 0 {
+		//use default table name
+	} else {
+		switch dbdialect {
+		case sqldb.Postgres, sqldb.Oracle:
+			dbtableName = dbschema + "." + tableName
+		default:
+			dbtableName = dbschema + tableName
+		}
+	}
+
+	placeholder := dbdialect.Placeholder()
+	primaryKeyFieldNames := pdbutil.GetPrimaryKeyFieldDescs(msgDesc, msgFieldDescs, false)
+
+	if len(primaryKeyFieldNames) == 0 {
+		return "", nil, fmt.Errorf("no primary key field")
+	}
+
+	sb := strings.Builder{}
+	sb.WriteString("with old as (select * from ")
+	sb.WriteString(dbtableName)
+	sb.WriteString(protosql.SQL_WHERE)
+
+	firstPlaceholder := true
+	sqlParaNo := 1
+
+	for fieldName := range primaryKeyFieldNames {
+		if firstPlaceholder {
+			firstPlaceholder = false
+		} else {
+			sb.WriteString(protosql.SQL_AND)
+		}
+
+		sb.WriteString(fieldName)
+		sb.WriteString(protosql.SQL_EQUEAL)
+
+		if placeholder == protosql.SQL_QUESTION {
+			sb.WriteString(string(protosql.SQL_QUESTION))
+		} else {
+			sb.WriteString(string(protosql.SQL_DOLLAR))
+			sb.WriteString(fmt.Sprint(sqlParaNo))
+			sqlParaNo++
+		}
+
+		val, err := pdbutil.GetField(msgobj, fieldName)
+		if err != nil {
+			return "", nil, err
+		}
+		sqlVals = append(sqlVals, val)
+	}
+
+	sb.WriteString(protosql.SQL_RIGHT_PARENTHESES)
+
+	sb.WriteString(protosql.SQL_UPDATE)
+	sb.WriteString(tableName)
+	sb.WriteString(" new ")
+	sb.WriteString(protosql.SQL_SET)
+
+	valFieldNames := make([]string, 0)
+	updateFieldsMap := make(map[string]bool)
+	for _, fieldName := range updateFields {
+		updateFieldsMap[fieldName] = true
+	}
+
+	for fi := 0; fi < msgFieldDescs.Len(); fi++ {
+		field := msgFieldDescs.Get(fi)
+		fieldName := string(field.Name())
+
+		if _, ok := updateFieldsMap[fieldName]; !ok {
+			continue
+		}
+
+		if _, ok := primaryKeyFieldNames[fieldName]; ok {
+			continue
+		}
+
+		fieldPdb, _ := pdbutil.GetPDB(field)
+		if !fieldPdb.NeedInUpdate() {
+			continue
+		}
+
+		valFieldNames = append(valFieldNames, fieldName)
+
+		val, err := pdbutil.GetField(msgobj, fieldName)
+		if err != nil {
+			err = fmt.Errorf("get field err: %s.%s %w", msgDesc.Name(), fieldName, err)
+			return "", nil, err
+		}
+
+		isValZero := pdbutil.IsZeroValue(val)
+		_, hasDefaultValue := fieldPdb.HasDefaultValue()
+		if !fieldPdb.IsNotNull() && (fieldPdb.IsReference() || fieldPdb.IsZeroAsNull()) && isValZero {
+			val = pdbutil.NullValue
+		} else if isValZero && hasDefaultValue {
+			val = fieldPdb.DefaultValue2SQLArgs()
+		}
+
+		sqlVals = append(sqlVals, val)
+	}
+
+	if len(valFieldNames) == 0 {
+		return "", nil, fmt.Errorf("no field need update")
+	}
+
+	firstPlaceholder = true
+	for _, fieldName := range valFieldNames {
+		if firstPlaceholder {
+			firstPlaceholder = false
+		} else {
+			sb.WriteString(protosql.SQL_COMMA)
+		}
+
+		sb.WriteString(fieldName)
+		sb.WriteString(protosql.SQL_EQUEAL)
+
+		if placeholder == protosql.SQL_QUESTION {
+			sb.WriteString(string(protosql.SQL_QUESTION))
+		} else {
+			sb.WriteString(string(protosql.SQL_DOLLAR))
+			sb.WriteString(fmt.Sprint(sqlParaNo))
+			sqlParaNo++
+		}
+	}
+
+	sb.WriteString(protosql.SQL_FROM)
+	sb.WriteString(" old ")
+	sb.WriteString(protosql.SQL_WHERE)
+
+	firstPlaceholder = true
+	for fieldName := range primaryKeyFieldNames {
+		if firstPlaceholder {
+			firstPlaceholder = false
+		} else {
+			sb.WriteString(protosql.SQL_AND)
+		}
+
+		sb.WriteString(" new.")
+		sb.WriteString(fieldName)
+		sb.WriteString(protosql.SQL_EQUEAL)
+		sb.WriteString("old.")
+		sb.WriteString(fieldName)
+	}
+
+	sb.WriteString(protosql.SQL_RETURNING)
+	sb.WriteString(" old.*,new.*")
+	sb.WriteString(protosql.SQL_SEMICOLON)
+
+	return sb.String(), sqlVals, nil
 }
