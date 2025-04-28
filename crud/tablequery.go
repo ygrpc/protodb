599c1d1fadaabc0c0b6f660a345ffa8043862467
yangjun <290968698@qq.com>
2025-04-28 15:46:10 +0800

    add = in where2 for function call

--- crud/tablequery.go
+++ crud/tablequery.go
@@ -151,15 +151,17 @@ func TableQueryBuildSql(db *sql.DB, tableQueryReq *protodb.TableQueryReq, permis
 func FieldOperator2Str(fieldop protodb.WhereOperator) string {
 	switch fieldop {
 	case protodb.WhereOperator_WOP_GT:
-		return " > "
+		return protosql.SQL_GT
 	case protodb.WhereOperator_WOP_LT:
-		return " < "
+		return protosql.SQL_LT
 	case protodb.WhereOperator_WOP_GTE:
-		return " >= "
+		return protosql.SQL_GTE
 	case protodb.WhereOperator_WOP_LTE:
-		return " <= "
+		return protosql.SQL_LTE
 	case protodb.WhereOperator_WOP_LIKE:
-		return " LIKE "
+		return protosql.SQL_LIKE
+	case protodb.WhereOperator_WOP_EQ:
+		return protosql.SQL_EQUEAL
 	default:
 		return " unsupported operator: " + fmt.Sprint(fieldop)
 	}
