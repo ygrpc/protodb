f27e951d5f7b1ea316f62ac0fff81a0da8d5fc4d
yangjun <290968698@qq.com>
2025-05-28 18:57:38 +0800

    boolean no need quote

--- ddl/create.go
+++ ddl/create.go
@@ -32,10 +32,13 @@ func TryAddQuote2DefaultValue(fieldType protoreflect.Kind, defaultValue string) 
 		protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind,
 		protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
 		protoreflect.Uint64Kind, protoreflect.Fixed64Kind,
-		protoreflect.FloatKind, protoreflect.DoubleKind:
+		protoreflect.FloatKind, protoreflect.DoubleKind,
+		//bool
+		protoreflect.BoolKind:
 		{
 			return defaultValue
 		}
+
 	}
 
 	trimmedDefaultValue := strings.TrimSpace(defaultValue)
